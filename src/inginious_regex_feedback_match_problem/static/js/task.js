5fc86d3e3a4af893397d273a78da906e5be88ffa
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2025-08-20 02:53:11 +0200

    Add missing load_input_regex_feedback_match

--- src/inginious_regex_feedback_match_problem/static/js/task.js
+++ src/inginious_regex_feedback_match_problem/static/js/task.js
@@ -1,3 +1,11 @@
 function load_feedback_regex_feedback_match(key, content) {
     load_feedback_code(key, content);
 }
+
+function load_input_regex_feedback_match(submissionid, key, input) {
+    var field = $(".problem input[name='" + key + "']");
+    if(key in input)
+        $(field).prop('value', input[key]);
+    else
+        $(field).prop('value', "");
+}
